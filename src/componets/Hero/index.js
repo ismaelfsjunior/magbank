ef70be269697dcb3e0fae79ee806f358a13057db
ismael junior <ifsjunior2@gmail.com>
2022-10-23 19:50:39 -0300

    feat(HERO): add component Card.

--- src/componets/Hero/index.js
+++ src/componets/Hero/index.js
@@ -1,25 +1,25 @@
 import React from "react";
-import { Row, Col, Button, Image } from "react-bootstrap";
+import { Card, Row, Col, Button, Image, Container } from "react-bootstrap";
 //import hero from "../../assets/hero.jpg";
 import logo from "../../assets/logo__magic-pay--mobile.png";
 import logoDesktop from "../../assets/logo__magic-pay.png";
 import "./index.scss";
 
 const Hero = () => (
-        // <Container>
-                <div className="jumobtron text-center text-light hero">
-                    <Row>
-                        <Col lg className="text-lg-end my-lg-5">
-                            <Image className="d-lg-none" src={logo} />
-                            <Image className="d-none d-lg-inline-block" src={logoDesktop} />
-                        </Col>
-                        <Col lg className="text-lg-start my-lg-5">
-                            <p>Pague suas contas pelo nosso APP</p>
-                            <Button variant="outline-light">Abra sua conta</Button>
-                        </Col>
-                    </Row>
-                </div>     
-        // </Container>     
+    <Card className="text-center text-white hero">
+        <Container>
+            <Row>
+                <Col lg className="text-lg-end my-lg-5">
+                    <Image className="d-lg-none" src={logo} />
+                    <Image className="d-none d-lg-inline-block" src={logoDesktop} />
+                </Col>
+                <Col lg className="text-lg-start my-lg-5 fs-5">
+                    <p>Pague suas contas pelo nosso APP</p>
+                    <Button variant="outline-light">Abra sua conta</Button>
+                </Col>
+            </Row>
+        </Container>
+    </Card>     
 );
 
 export default Hero;
