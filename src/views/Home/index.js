ff95897396bfd9fe99e4e719912c94e8db5aabde
ismael junior <ifsjunior2@gmail.com>
2022-11-06 16:46:04 -0300

    feat(ROUTER): Created routes.

---
+++ src/views/Home/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Hero from "../../componets/Hero/index";
+import Main from "../../componets/Main/index.js";
+import Institutional from "../../componets/Institutional/index.js";
+import Faq from "../../componets/Faq/index.js";
+
+const Home = () => {
+
+  return (
+    <>  
+      <Hero />
+      <Main />
+      <Institutional />
+      <Faq />
+    </>
+  );
+}
+
+export default Home;
