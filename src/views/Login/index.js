6f95a80f36b3317232ffd022a06bf76faf95337a
ismael junior <ifsjunior2@gmail.com>
2022-11-06 17:00:30 -0300

    feat(LOGIN): ajust elements.

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Row, Col, Form, Image, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import logo from '../../assets/logo.svg';
 import './index.scss';
 
@@ -8,21 +9,27 @@ const Login = () => {
         <section className="login">
             <div className="login__container">
                 <Row>
-                    <Col className="text-center">
+                    <Col className="text-center text-light">
                         <Image src={logo} className="mb-5"/>
                         <Form>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>Número da Conta</Form.Label>
+                            <Form.Group className="mb-3" controlId="formBasicNumber">
+                                <Form.Label className="lead">
+                                    <strong>Número da Conta</strong>
+                                </Form.Label>
                                 <Form.Control type="number" />
                             </Form.Group>
 
                             <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>Senha</Form.Label>
+                                <Form.Label className="lead">
+                                    <strong>Senha</strong>
+                                </Form.Label>
                                 <Form.Control type="password"/>
                             </Form.Group>
-                            <Button variant="success" type="submit">
-                                Criar Conta
-                            </Button>
+                                <Link to={"/dashboard"}>
+                                    <Button className="mt-3" variant="success" type="submit">
+                                        Criar Conta
+                                    </Button>
+                                </Link>
                         </Form>
                     </Col>
                 </Row>                
