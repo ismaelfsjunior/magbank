666736f9eadaab6bd55df3997967a0b26fb672de
ismael junior <ifsjunior2@gmail.com>
2022-11-20 08:30:03 -0300

    feat(DASHBOARD): created page dashboard.

--- src/componets/Footer/index.js
+++ src/componets/Footer/index.js
@@ -9,16 +9,16 @@ import "./index.scss";
 const Footer = () => {
     return(
         <footer className="footer">
-            <Container className="text-center py-5">
-                <Row className="d-flex align-items-center justify-items-center">
-                    <Col xs={12} lg={6} className="mb-5 mb-md-0">
+            <Container>
+                <Row className="text-center py-5">
+                    <Col xs={12} lg={6} className="text-lg-start mb-5 mb-lg-0">
                         <Image src={logo}/>
                     </Col>
-                    <Col xs={12} lg={4} className="mb-5 mb-md-0" >
+                    <Col xs={12} lg={4} className="mb-5 mb-lg-0" >
                         <Image src={appstore} className="me-3"/>
                         <Image src={googleplay}/>
                     </Col>
-                    <Col xs={12} lg={2} className="fs-1">
+                    <Col xs={12} lg={2} className="fs-1 d-flex align-items-center justify-content-center">
                         <FaFacebook className="me-2" color="#7c7c7c"/>
                         <FaTwitter className="me-2" color="#7c7c7c"/>
                         <FaYoutube color="#7c7c7c"/>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../componets/Home";
 import Login from "../views/Login";
+import Dashboard from "../views/Dashboard";
 
 export default function Rotas() {
     return(
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={ <Login />} />
+            <Route path="/dashboard" element={ <Dashboard />} />
         </Routes>
     );
 }
